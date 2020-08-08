95ad93c9601687172e979b2dd18301a12681874e
matt-green1 <60438349+matt-green1@users.noreply.github.com>
2020-08-08 09:56:02 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
 // Write your code here
+
+
+let num1 = 2
+let num2 = 31
+
+let multiply = num1 * num2
+
+
+let random = (Math.random() * 1000) + 1 
+
+
+let mod = 25%7
+
+let max = Math.max(1, 3, 5, 20)
